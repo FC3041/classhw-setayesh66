4fe864b5ba45282dd0b09f83fd2f89c09e6c1aba
Setayesh <whatifimurder@gmail.com>
2025-03-09 23:42:48 +0330

    stringreverse

---
+++ S8/s8con.Test/Test1.cs
@@ -0,0 +1,18 @@
+﻿namespace s8con.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        int x = 1;
+        Assert.AreEqual(x+1, 2);
+    }
+    public void TestMethod2()
+    {
+        int result = Program.add(3,5);
+        Assert.AreEqual(result, 8);
+    }
+
+}
---
+++ S8/s8con/Program.cs
@@ -0,0 +1,53 @@
+﻿// using System.ComponentModel;
+// using System.ComponentModel.Design.Serialization;
+
+
+
+namespace s8con;
+// using System;
+public class Program
+{
+    public static void reverse(string s, out string srev)
+    {
+
+    }
+    public static int add(int a, int b)
+    {
+        return a+b;
+    }
+
+    public void prepend_user(string s, out string  sout)
+    {
+        sout = "user:"+s;
+    }
+
+
+
+
+    static void Main(string[] args)
+    {
+        int sum =0;
+        while (true)
+        {
+            System.Console.WriteLine("Nums?");
+            string s = Console.ReadLine();
+            if (string.IsNullOrEmpty(s))
+                break;
+            
+            // sum += int.Parse(s);
+            int n;
+            if (! int.TryParse(s, out n))
+            {
+                System.Console.WriteLine("wrong");
+                continue;
+            }
+            string sout;
+            prepend_user(sum, out sout);
+            sum += n;
+            
+            
+        }
+
+        System.Console.WriteLine(sum);
+    }
+}
